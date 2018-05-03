1eb77b8f7c6af2eb62183a614fd86a6ee305a99c
JonasMoens <32325942+JonasMoens@users.noreply.github.com>
2018-05-03 15:22:01 +0200

    Update scriptquery1.sh

--- experiment/scripts/scriptquery1.sh
+++ experiment/scripts/scriptquery1.sh
@@ -3,9 +3,9 @@
 #This script will execute query1 multiple (40) times.
 
 i=1
-echo "real" >> /vagrant/uitvoer/UitvoerQuery1.csv
+echo "real" >> /vagrant/uitvoer/Real Execution Time MariaDB/UitvoerQuery1.csv
 while [ $i -le 40 ]
 do
-( /usr/bin/time -f "%e" mysql -udbo -pdbo dbo < /vagrant/queries/query1.sql > /dev/null ) 2>> /vagrant/uitvoer/UitvoerQuery1.csv
+( /usr/bin/time -f "%e" mysql -udbo -pdbo dbo < /vagrant/queriesMariaDB/query1.sql > /dev/null ) 2>> /vagrant/uitvoer/Real Execution Time MariaDB/UitvoerQuery1.csv
 i=$[$i+1]
 done
